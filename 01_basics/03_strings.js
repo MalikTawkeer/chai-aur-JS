175521f07ee0b5ef434aa44391252a4293fc79aa
malik towkeer ul islam <108170042+MalikTawkeer@users.noreply.github.com>
2023-10-06 18:47:00 +0000

    adding strings

--- 01_basics/03_strings.js
+++ 01_basics/03_strings.js
@@ -6,3 +6,12 @@ const repoCount = 12
 //modern approach STRING INTERPOLATION uses bactics
 console.log(`my name is ${name.toUpperCase()} and repocount is ${repoCount}`)
 
+
+
+const str = new String('malika')
+// console.log(str.length);
+// console.log(str.toLocaleUpperCase())
+// console.log(str.charAt(2))
+// console.log(str.indexOf('a'))
+
+console.log(str.substring(0, 3))
